19741141927d10fe851918217ffb3576a1052686
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2026-09-04 12:44:43 +0200

    inital

---
+++ Ducks/DecoyDuck.cs
@@ -0,0 +1,26 @@
+﻿using StrategyPattern.Interfaces.QuackBehavior;
+using StrategyPattern.Interfaces.FlyBehavior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.Ducks
+{
+    internal class DecoyDuck : Duck
+    {
+        public DecoyDuck()
+        {
+            quackBehavior = new MuteQuack();
+            flyBehavior = new FlyNoWay();
+        }
+        public override void Display()
+        {
+            Console.WriteLine("I'm a Decoy Duck");
+        }
+        
+
+
+    }
+}
